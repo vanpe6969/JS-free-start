efd12bebbf2cacbe1f6d2f14d90ec6f5b4a001d2
vanpe <vanpe.washswat@gmail.com>
2020-01-13 22:06:09 +0900

    #1.7 let, const, var.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Im Working. Im JS. Im Beautiful. Im worth it");
+const a = 221 - 5;
+let b = a - 5;
+a = 4;
+console.log(b, a);
+<iframe
+  width="1150"
+  height="647"
+  src="https://www.youtube.com/embed/ZTJjW7XuHIY"
+  frameborder="0"
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen
+></iframe>;
