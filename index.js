254ff3e04630716a500b38a449600c853feceea8
vanpe <vanpe.washswat@gmail.com>
2020-01-14 22:14:10 +0900

    1-8,1-10Study.html

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const a = 221 - 5;
-let b = a - 5;
-a = 4;
-console.log(b, a);
-<iframe
-  width="1150"
-  height="647"
-  src="https://www.youtube.com/embed/ZTJjW7XuHIY"
-  frameborder="0"
-  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-  allowfullscreen
-></iframe>;
+const heeSooFuck = {
+  name: "Heesoo",
+  age: 31,
+  gender: "Male",
+  isHandsome: true
+};
+
+console.log(heeSooFuck.gender);
