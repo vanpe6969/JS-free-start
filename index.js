fbcd1e3b31617490375ac54f4451dd43490778c8
vanpe <vanpe.washswat@gmail.com>
2020-01-16 22:29:10 +0900

    1/16 #2-2.html
    function, return   shit........

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-function sayHello(heesoo, una) {
-  console.log(
-    "Hello Shit!",
-    heesoo,
-    "fuck the shit",
-    una,
-    "years of age.",
+const calculator = {
+  sum: function(a, b) {
+    return a + b;
+  },
+  sub: function(a, b) {
+    return a - b;
+  },
+  mul: function(a, b) {
+    return a * b;
+  },
+  div: function(a, b) {
+    return a / b;
+  },
+  pow: function(a, b) {
+    return a ** b;
+  }
+};
 
-    "olaliggolali"
-  );
-}
+const sum = calculator.sum(5, 5);
+const sub = calculator.sub(8, 7);
+const mul = calculator.mul(3, 2);
+const div = calculator.div(8, 2);
+const pow = calculator.pow(6, 3);
 
-sayHello("una", 10);
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(pow);
